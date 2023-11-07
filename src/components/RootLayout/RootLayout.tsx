e27f1a9d91941e279b54cfc14bca8f9f6ac27535
Darya <watashi.no.kao24@gmail.com>
2023-11-07 23:12:01 +0300

    feat: uncomment useful code

--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -1,16 +1,16 @@
-import './RootLayout.module.css';
-import '../../index.css';
-import React, { FC, useEffect, useState } from 'react';
+import "./RootLayout.module.css";
+import "../../index.css";
+import React, { FC, useEffect, useState } from "react";
 
-import Header from '../Header/Header';
-import Pagination from '../Pagination/Pagination';
-import Main from '../Main/Main';
-import { MainProps } from '../../Interfaces/Interfaces';
-import { Outlet, useSearchParams } from 'react-router-dom';
+import Header from "../Header/Header";
+import Pagination from "../Pagination/Pagination";
+import Main from "../Main/Main";
+import { MainProps } from "../../Interfaces/Interfaces";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 const RootLayout: FC = () => {
   const [isDetails, setIsDetails] = useState(false);
-  const [request, setRequest] = useState<string>('');
+  const [request, setRequest] = useState<string>("");
   const [error, setError] = useState<boolean>(false);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [pageSize, setPageSize] = useState<number>(10);
@@ -20,15 +20,15 @@ const RootLayout: FC = () => {
     data: [],
     info: {
       count: 0,
-      nextPage: '',
-      previousPage: '',
+      nextPage: "",
+      previousPage: "",
       totalPages: 0,
     },
   });
 
   function fetchRequest(request: string, page: number, pageSize: number) {
     fetch(
-      `https://api.disneyapi.dev/character?name=${request}&page=${page}&pageSize=${pageSize}`
+      `https://api.disneyapi.dev/character?name=${request}&page=${page}&pageSize=${pageSize}`,
     )
       .then((res) => res.json())
       .then((result: MainProps) => {
@@ -45,10 +45,10 @@ const RootLayout: FC = () => {
 
         setIsLoaded(true);
         setError(false);
-        // if (result.info.count === 0) {
-        //   setError(true);
-        //   localStorage.setItem('prevRequest', '');
-        // }
+        if (result.info.count === 0) {
+          setError(true);
+          localStorage.setItem("prevRequest", "");
+        }
       })
       .catch(() => {
         setIsLoaded(true);
@@ -59,19 +59,19 @@ const RootLayout: FC = () => {
     setSearch({
       page: `${page}`,
     });
-    const prevRequest = localStorage.getItem('prevRequest');
+    const prevRequest = localStorage.getItem("prevRequest");
     setIsLoaded(false);
 
-    setRequest(prevRequest || '');
-    fetchRequest(prevRequest || '', page, pageSize);
+    setRequest(prevRequest || "");
+    fetchRequest(prevRequest || "", page, pageSize);
   }, [request, page, pageSize]); // eslint-disable-line react-hooks/exhaustive-deps
 
   function handlePage(page: string) {
     switch (page) {
-      case 'next':
+      case "next":
         setPage((count) => count + 1);
         break;
-      case 'prev':
+      case "prev":
         setPage((count) => count - 1);
         break;
     }
@@ -82,23 +82,23 @@ const RootLayout: FC = () => {
   }
   function handleRequest(request: string) {
     setRequest(request);
-    localStorage.setItem('prevRequest', request.trim());
+    localStorage.setItem("prevRequest", request.trim());
   }
   if (error) {
-    throw new Error('Request not found');
+    throw new Error("Request not found");
   }
   function handleDetails() {
     setIsDetails(false);
   }
 
   return (
-    <div className={'app'}>
+    <div className={"app"}>
       <Header handleRequest={handleRequest} />
       {!isLoaded ? (
         <img
-          className={'loading'}
+          className={"loading"}
           src="/src/assets/loading.gif"
-          alt={'loading'}
+          alt={"loading"}
         />
       ) : (
         <>
@@ -109,7 +109,7 @@ const RootLayout: FC = () => {
             pageSize={pageSize}
             handlePageSize={handlePageSize}
           />
-          <div className={isDetails ? 'open-details' : ''}>
+          <div className={isDetails ? "open-details" : ""}>
             <Main
               setIsDetails={setIsDetails}
               results={results.data}
